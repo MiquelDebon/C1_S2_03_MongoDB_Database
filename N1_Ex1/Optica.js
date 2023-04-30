cb1dbf7dbd12ac6902aa21a99711d9849fe3d590
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-04-30 21:38:43 +0200

    First commit

---
+++ N1_Ex1/Optica.js
@@ -0,0 +1,41 @@
+db.createCollection('product',{
+    validator: {
+        $jsonSchema : {
+            bsonType: "object",
+            required : ["brand", "graduation", "frame", "frame_color", "glass_color", "price", "supplier_id"],
+            properties : {
+                brand : {
+                    bsonType : "string",
+                    description : "this should be an string type and is required"
+                },
+                graduation :{
+                    bsonType : "int",
+                    description : "this should be an int type and is required"
+                },
+                frame :{
+                    bsonType : "string",
+                    description : "this should be a string type and is required"
+                },
+                frame_color : {
+                    bsonType : "string",
+                    description : "this should be a string type and is required"
+                },
+                glass_color : {
+                    bsonType : "string",
+                    description : "this should be a string type and is required"
+                },
+                price : {
+                    bsonType : "int",
+                    description : "this should be an int type and is required"
+                },
+                supplier_id : {
+                    bsonType : "int",
+                    description : "this should be an int type and is required"
+                }
+
+            }
+
+        }
+    }
+
+})
